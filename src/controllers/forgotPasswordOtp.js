c3e5cc5bd51cc398ee96d8c6e9a4957364225967
miteshp2110 <paliwalmitesh2110@gmail.com>
2024-12-29 17:08:48 +0530

    Added forgot Password controller

---
+++ src/controllers/forgotPasswordOtp.js
@@ -0,0 +1,58 @@
+//This controller will manage sending the otp for forgot password.
+
+const getOtp = require('../utils/otpGenerator')
+const {dataEncrypter} = require('../utils/dataEncrypter')
+const existingUser = require("../utils/existingUser")
+const redis = require('../config/redisClient')
+
+const forgotPasswordOtp = (async(req,res)=>{
+
+    const {email} = req.body
+
+    try{
+        if(!email){
+            res.status(400).json({"Message":"Email not provided"})
+        }
+        if(! await existingUser(email,false)){
+            return res.status(400).json({"Message":"User not found"})
+        }
+    
+        var cache = JSON.parse(await redis.get(`auth:forgot:user:${email}`))
+    
+        if(cache){
+        
+            if(cache.attemptsRemaining > 0){
+                let otp = await getOtp()
+                console.log("attempt otp: ",otp)
+                let data = {
+                    email:email,
+                    otp:await dataEncrypter(otp),
+                    attemptsRemaining:cache.attemptsRemaining-1
+                }
+                await redis.set(`auth:forgot:user:${email}`,JSON.stringify(data),'EX',300)
+                return res.status(200).json({"Message":`Otp sent to ${email}`})
+
+            }
+            return res.status(429).json({"Message":"Too many attempts, try again in 5 minutes."})
+
+        }
+        else{
+            let otp = await getOtp()
+            console.log("first otp: ",otp)
+            let data = {
+                email:email,
+                otp:await dataEncrypter(otp),
+                attemptsRemaining:3
+            }
+            await redis.set(`auth:forgot:user:${email}`,JSON.stringify(data),'EX',300)
+            return res.status(200).json({"Message":`Otp sent to ${email}`})
+        }
+
+    }
+    catch(err){
+        console.log(err)
+    }
+
+})
+
+module.exports = forgotPasswordOtp
--- src/routes/basicAuthRoutes.js
+++ src/routes/basicAuthRoutes.js
@@ -4,10 +4,12 @@ const router = express.Router()
 const userSignUp = require('../controllers/userSignUp')
 const validateOTP = require('../controllers/validateOTP')
 const userLogin = require("../controllers/userLogin")
+const forgotPasswordOtp = require('../controllers/forgotPasswordOtp')
 
 router.post("/api/signup",userSignUp)
 router.post("/api/validateOtp",validateOTP)
 router.post("/api/login",userLogin)
+router.post("/api/forgotPassword",forgotPasswordOtp)
 
 
 
